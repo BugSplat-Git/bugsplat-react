76aba7bbb61ffc11d55c66e5f4abd1ec307b9109
Zach <zmriel@gmail.com>
2022-06-02 15:34:03 -0400

    feat: error boundary integration test

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,12 @@
 /* eslint-env node */
+
+/**
+ * @type {import('@jest/types').Config.InitialOptions}
+ */
 module.exports = {
   coverageProvider: 'v8',
+  globals: { fetch, FormData, Blob },
+  preset: 'ts-jest',
   setupFilesAfterEnv: ['<rootDir>/src/setupTests.ts'],
   testEnvironment: 'jsdom',
-  preset: 'ts-jest',
 };
---
+++ src/__integration_tests__/ErrorBoundaryPostsToCrashApi.test.tsx
@@ -0,0 +1,112 @@
+import {
+  BugSplatApiClient,
+  CrashApiClient,
+  Environment,
+} from '@bugsplat/js-api-client';
+import { render, waitFor, screen } from '@testing-library/react';
+import { BugSplatResponse } from 'bugsplat';
+import { BugSplatResponseBody } from 'bugsplat/dist/cjs/bugsplat-response';
+import { getBugSplat, init } from '../core';
+import ErrorBoundary from '../ErrorBoundary';
+
+const email = 'fred@bugsplat.com';
+// const password = process.env.FRED_PASSWORD;
+const password = 'Flintstone';
+const appBaseUrl = 'https://app.bugsplat.com';
+
+const BlowUpError = new Error('Error thrown during render.');
+
+function BlowUp(): JSX.Element {
+  throw BlowUpError;
+}
+
+describe('<ErrorBoundary />', () => {
+  let client: CrashApiClient;
+
+  beforeEach(async () => {
+    if (!password) {
+      throw new Error('`FRED_PASSWORD` environment variable must be set!');
+    }
+    const apiClient = new BugSplatApiClient(appBaseUrl, Environment.Node);
+    await apiClient.login(email, password);
+    client = new CrashApiClient(apiClient);
+  });
+
+  it('should post a crash report with all the provided information', async () => {
+    const database = 'fred';
+    const application = 'my-react-crasher';
+    const version = '3.2.1';
+    const appKey = 'Key!';
+    const user = 'User!';
+    const email = 'fred@bedrock.com';
+    const description = 'Description!';
+
+    init({
+      database,
+      application,
+      version,
+      defaultProps: {
+        appKey,
+        user,
+        email,
+        description,
+      },
+    });
+
+    const bugSplat = getBugSplat() as unknown as {
+      _formData: () => FormData;
+      _fetch: typeof fetch;
+    };
+
+    bugSplat._formData = () => new globalThis.FormData();
+    bugSplat._fetch = globalThis.fetch;
+
+    let result: BugSplatResponse | null;
+
+    render(
+      <ErrorBoundary
+        onError={(_e, _c, response) => {
+          result = response;
+        }}
+        fallback={({ response }) => (
+          <div role="alert">
+            {(response?.response as BugSplatResponseBody)?.crash_id}
+          </div>
+        )}
+      >
+        <BlowUp />
+      </ErrorBoundary>
+    );
+
+    let alert: HTMLDivElement;
+    let expectedCrashId = -1;
+
+    await waitFor(async () => {
+      alert = await screen.findByRole<HTMLDivElement>('alert');
+
+      expect(alert).toBeInTheDocument();
+    });
+
+    await waitFor(() => {
+      if (result?.error !== null) {
+        throw result?.error;
+      }
+
+      expectedCrashId = result.response.crash_id;
+    });
+
+    const crashData = await client.getCrashById(database, expectedCrashId);
+
+    expect(crashData.appName).toEqual(application);
+    expect(crashData.appVersion).toEqual(version);
+    expect(crashData.appKey).toEqual(appKey);
+    expect(crashData.description).toEqual(description);
+
+    /**
+     * Fred has PII obfuscated so the best
+     * we can do for these is to check if truthy
+     */
+    expect(crashData.user).toBeTruthy();
+    expect(crashData.email).toBeTruthy();
+  });
+});
