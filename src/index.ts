23b330a3df3762123040a7a2269d1a9b21ab2027
Zach <zmriel@gmail.com>
2022-06-10 14:41:17 -0400

    chore: update imports

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
-export * from './ErrorBoundary';
-export { default as withErrorBoundary } from './withErrorBoundary';
-export { default as useErrorHandler } from './useErrorHandler';
-
 // re-export everything from bugsplat-js
 export * from 'bugsplat';
+export * from './BugSplatScope';
+export * from './ErrorBoundary';
+export { default as useErrorHandler } from './useErrorHandler';
+export { default as withErrorBoundary } from './withErrorBoundary';
+
