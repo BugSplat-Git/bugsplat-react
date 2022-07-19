d57f502c8818d0510429ce77778e76c4f9236f11
Zach <zmriel@gmail.com>
2022-07-19 11:35:20 -0400

    fix!: only export types from bugsplat

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,10 @@
-// re-export everything from bugsplat-js
-export * from 'bugsplat';
+// re-export types from bugsplat-js
+export type {
+  BugSplat,
+  BugSplatOptions,
+  BugSplatResponse,
+  FormDataParam,
+} from 'bugsplat';
 export * from './appScope';
 export * from './ErrorBoundary';
 export { default as useErrorHandler } from './useErrorHandler';
