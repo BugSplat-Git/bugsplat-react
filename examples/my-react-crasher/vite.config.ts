4438471b914fb1426b307400dd114c4f8eddde46
Zach <zmriel@gmail.com>
2022-08-29 16:54:00 -0400

    fix(examples): set sourcemap build option

--- examples/my-react-crasher/vite.config.ts
+++ examples/my-react-crasher/vite.config.ts
@@ -1,11 +1,14 @@
 /// <reference types="vitest" />
 
-import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
 
 export default defineConfig({
   plugins: [react()],
   envPrefix: 'BUGSPLAT_',
+  build: {
+    sourcemap: true,
+  },
   test: {
     environment: 'jsdom',
     include: ['./src/**/*.{test,spec}.{ts,tsx}'],
