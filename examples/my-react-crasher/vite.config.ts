ab0c1fab702a0ae1091f6fa6883f742f0c186c5a
Zach <zmriel@gmail.com>
2022-09-07 09:40:26 -0400

    fix(examples): pages build with subdir

--- examples/my-react-crasher/vite.config.ts
+++ examples/my-react-crasher/vite.config.ts
@@ -1,17 +1,20 @@
-/// <reference types="vitest" />
+/// <reference types="./vite-env" />
 
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 
-export default defineConfig({
-  base: "/my-react-crasher/",
-  plugins: [react()],
-  envPrefix: 'BUGSPLAT_',
-  build: {
-    sourcemap: true,
-  },
-  test: {
-    environment: 'jsdom',
-    include: ['./src/**/*.{test,spec}.{ts,tsx}'],
-  },
+export default defineConfig(({mode}) => {
+  return {
+    base: mode === "pages" ? "/my-react-crasher/" : "/",
+    plugins: [react()],
+    envPrefix: 'BUGSPLAT_',
+    build: {
+      sourcemap: true,
+    },
+    test: { 
+      environment: 'jsdom',
+      include: ['./src/**/*.{test,spec}.{ts,tsx}'],
+    },
+  }
 });
+  
