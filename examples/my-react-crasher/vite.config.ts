eea1919e040606546543d41d4163c66f250067fa
Zach <zmriel@gmail.com>
2022-09-07 07:10:18 -0400

    fix(my-react-crasher): add base url to vite config

--- examples/my-react-crasher/vite.config.ts
+++ examples/my-react-crasher/vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+  base: "/my-react-crasher/",
   plugins: [react()],
   envPrefix: 'BUGSPLAT_',
   build: {
